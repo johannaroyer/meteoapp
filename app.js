1cfc4cc42f1a08e1d047e0d545ea2aad366b89c2
Johanna Royer <johannaroyer@orange.fr>
2020-03-11 10:43:05 +0100

    envoi app meteo

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY
+
 const express = require("express")
 const app = express()
 const axios = require("axios")
 
-app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
 
-// const pokemonFaker = {
-//     name: "faker",
-//     id: 300
-// }
-
-app.get("/", function(req, res){
-
-    axios.get("https://pokeapi.co/api/v2/pokemon/ditto/")
-    .then(response => {
-        console.log(response)
-        res.render("index", { pokemon: response.data })
-    })
-
-    // res.render("index", {pokemon: pokemonFaker })
-
+app.get('/', function(req, res){
+    res.render('index')
 })
 
-app.get("/contact", function(req, res){
-    res.render("pages/contact")
+
+app.post('/weather', (req, res) => {
+    const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`
+    console.log(req.body)
+    axios({
+        url: url,
+        responseType : 'json'
+    }).then(data => res.json(data.data.currently))
 })
 
-app.listen(8080)
+
+app.listen(8000, () => {
+    console.log('Serveur lancé')
+})
